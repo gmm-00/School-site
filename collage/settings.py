a8b2336ac1097c1dec09d056bef6a946d27bb4df
Ashok <gaireashok36@gmail.com>
2023-08-10 00:36:59 +0545

    updated

--- collage/settings.py
+++ collage/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'collage.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "USER": "ashok",
+        "PASSWORD": "ashok123",
+        "NAME":"tilottama"
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
